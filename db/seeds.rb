14cc5781bc8d9bfb04213ef30abccc23cb09cd6b
Dan Shapiro <ds.dan.shapiro@gmail.com>
2019-06-09 16:05:07 -0400

    Implemented edit and update functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ puts "5 skills created"
 9.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}",
-		subtitle: "My greate service",
+		subtitle: "My great service",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie erat lorem, ut blandit nulla fringilla in. Maecenas ultricies ut augue et egestas. Integer viverra neque et semper posuere. Phasellus feugiat placerat tortor vel rhoncus. Aliquam feugiat, nisi a fringilla commodo, dui enim dignissim turpis, sed pulvinar lorem ipsum non augue. Donec in venenatis purus. Donec in lectus nec tortor iaculis tempor sed eu est.",
 		main_image: "https://via.placeholder.com/600x400",
 		thumb_image: "https://via.placeholder.com/350x200"
